6e60aeeae5bd3fb536ff60ec055605e0dc36478e
shanhexi <wdbb8848@163.com>
2023-06-23 18:07:32 +0800

    fix:iconfont

--- chat2db-client/src/components/Iconfont/index.tsx
+++ chat2db-client/src/components/Iconfont/index.tsx
@@ -1,20 +1,37 @@
 import React, { PureComponent } from 'react';
 import classnames from 'classnames';
-import desktopStyle from './desktop.less';
-import prodStyle from './prod.less';
+// import desktopStyle from './desktop.less';
+import styles from './index.less';
 
-export default class Iconfont extends PureComponent<
-  {
-    code: string;
-  } & React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>
-> {
+let container = ''
+if (window._ENV === 'desktop') {
+  container = `
+    @font-face {
+      font-family: 'iconfont'; /* Project id 3633546 */
+      src: url('../../assets/font/iconfont.woff2') format('woff2'),
+        url('../../assets/font/iconfont.woff') format('woff'),
+        url('../../assets/font/iconfont.ttf') format('truetype');
+    }
+  `
+} else {
+  container = `
+    @font-face {
+      font-family: 'iconfont';  /* Project id 3633546 */
+      src: url('//at.alicdn.com/t/c/font_3633546_bobs6jadiya.woff2?t=1687102726036') format('woff2'),
+          url('//at.alicdn.com/t/c/font_3633546_bobs6jadiya.woff?t=1687102726036') format('woff'),
+          url('//at.alicdn.com/t/c/font_3633546_bobs6jadiya.ttf?t=1687102726036') format('truetype');
+    }
+  `
+}
+let style = document.createElement("style");
+style.type = "text/css";
+document.head.appendChild(style);
+style.appendChild(document.createTextNode(container));
+
+export default class Iconfont extends PureComponent<{
+  code: string;
+} & React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>> {
   render() {
-    const styles = window._ENV !== 'prod' ? desktopStyle : prodStyle;
-    // const styles = prodStyle;
-    return (
-      <i {...this.props} className={classnames(this.props.className, styles.iconfont)}>
-        {this.props.code}
-      </i>
-    );
+    return <i {...this.props} className={classnames(this.props.className, styles.iconfont)}>{this.props.code}</i>
   }
 }
