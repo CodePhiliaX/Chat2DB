76ed736ef7c7ae959727bc8ab6c5a96ad9ffd360
单贺喜 <wb-dhx909739@alibaba-inc.com>
2023-06-25 16:40:25 +0800

    umi 环境变量

--- chat2db-client/.umirc.ts
+++ chat2db-client/.umirc.ts
@@ -45,6 +45,8 @@ export default defineConfig({
   headScripts: ['if (window.myAPI) { window.myAPI.startServerForSpawn() }'],
   favicons: ['logo.ico'],
   define: {
-    'process.env.UMI_ENV': process.env.UMI_ENV,
+    'process.env': {
+      'UMI_ENV': process.env.UMI_ENV
+    }
   }
 });
