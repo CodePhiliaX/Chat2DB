f8a103f51d6391a754aa8cc793f9b24198142c31
shanhexi <wdbb8848@163.com>
2023-06-27 22:14:59 +0800

    style: tabs

--- chat2db-client/src/components/Tabs/index.tsx
+++ chat2db-client/src/components/Tabs/index.tsx
@@ -1,37 +1,77 @@
-import React, { memo, ReactNode, useState } from 'react';
-import styles from './index.less';
+import React, { memo, useEffect, useState } from 'react';
 import classnames from 'classnames';
-import { Tabs as AntdTabs } from 'antd';
+import Iconfont from '@/components/Iconfont';
+import lodash from 'lodash'
+import styles from './index.less';
 
-export interface ITab {
-  label: ReactNode;
-  key: string;
+export interface IOption {
+  label: string;
+  value: number;
+}
+
+export interface IOnchangeProps {
+  type: 'add' | 'delete' | 'switch';
+  data?: IOption;
 }
 
 interface IProps {
   className?: string;
-  tabs: ITab[];
-  currentTab?: string;
-  onChange: (key: string, index: number) => void;
-  extra?: React.ReactNode
+  tabs: IOption[];
+  onChange: (key: IOption['value']) => void;
+  onEdit: (action: 'add' | 'remove', key?: IOption['value']) => void;
 }
 
-export default memo(function Tabs({ className, tabs, currentTab, onChange, extra }: IProps) {
-  function myChange(key: string) {
-    const index = tabs.findIndex(t => {
-      return t.key === key
-    })
-    onChange(key, index)
+export default memo<IProps>(function Tab(props) {
+  const { className, tabs, onChange, onEdit } = props;
+  const [internalTabs, setInternalTabs] = useState<IOption[]>(lodash.cloneDeep(tabs));
+  const [activeTab, setActiveTab] = useState(internalTabs[0]?.value);
+
+  useEffect(() => {
+    setInternalTabs(lodash.cloneDeep(tabs));
+  }, [tabs])
+
+  function deleteTab(data: IOption) {
+    const newTabs = internalTabs?.filter(t => t.value !== data.value);
+    setInternalTabs(newTabs);
+    onEdit('remove', data.value)
   }
 
-  return <div className={classnames(className, styles.box)}>
-    <AntdTabs
-      defaultActiveKey={currentTab}
-      onChange={myChange}
-      items={tabs}
-    />
-    <div className={styles.extra}>
-      {extra}
+  function changeTab(data: IOption) {
+    setActiveTab(data.value);
+    onChange(data.value)
+
+  }
+
+  function handelAdd() {
+    onEdit('add')
+  }
+
+  return <div className={classnames(styles.tab, className)}>
+    {
+      !!internalTabs?.length &&
+      <div className={styles.tabList}>
+        {
+          internalTabs.map(t => {
+            return <div
+              className={classnames(styles.tabItem, { [styles.activeTab]: t.value === activeTab })}
+              key={t.value}
+              onClick={changeTab.bind(null, t)}
+            >
+              <div className={styles.text}>
+                {t.label}
+              </div>
+              <div className={styles.icon} onClick={deleteTab.bind(null, t)}>
+                <Iconfont code='&#xe634;' />
+              </div>
+            </div>
+          })
+        }
+      </div>
+    }
+    <div className={styles.rightBox}>
+      <div className={styles.addIcon} onClick={handelAdd}>
+        <Iconfont code='&#xe631;'></Iconfont>
+      </div>
     </div>
-  </div>
+  </div >
 })
--- chat2db-client/src/pages/demo/index.tsx
+++ chat2db-client/src/pages/demo/index.tsx
@@ -4,35 +4,11 @@ import { Button, Steps } from 'antd'
 import { LoadingOutlined, SmileOutlined, SolutionOutlined, UserOutlined } from '@ant-design/icons';
 import styles from './index.less';
 import Setting from '@/blocks/Setting';
-import Tab from '@/components/Tab';
+import Tabs from '@/components/Tabs';
 
 
 export default function Demo() {
-  const tabs = [
-    {
-      label: 'components/121212',
-      value: 1
-    },
-    {
-      label: 'components/121212components/121212components/121212',
-      value: 2
-    },
-    {
-      label: 'components/121212components/121212components/121212components/121212components/121212components/121212',
-      value: 3
-    },
-    {
-      label: 'components/121212components/121212components/121212components/121212',
-      value: 4
-    },
-    {
-      label: 'components/121212',
-      value: 5
-    },
-  ]
-  return <div className={styles.page}>
-    <Tab
-      tabs={tabs}
-    />
+  return <div>
+
   </div>
 }
