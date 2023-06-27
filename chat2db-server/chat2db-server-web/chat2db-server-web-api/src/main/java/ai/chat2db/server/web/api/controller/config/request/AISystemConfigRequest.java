4b36d7dd6c22d4916418280c94d119d81a4aac96
robin <850379744@qq.com>
2023-06-27 21:59:14 +0800

    add azure openai support

--- chat2db-server/chat2db-server-web/chat2db-server-web-api/src/main/java/ai/chat2db/server/web/api/controller/config/request/AISystemConfigRequest.java
+++ chat2db-server/chat2db-server-web/chat2db-server-web-api/src/main/java/ai/chat2db/server/web/api/controller/config/request/AISystemConfigRequest.java
@@ -13,7 +13,7 @@ import lombok.Data;
  * @version : SystemConfigRequest.java
  */
 @Data
-public class ChatGptSystemConfigRequest {
+public class AISystemConfigRequest {
 
     /**
      * OpenAi APIKEY
@@ -56,4 +56,19 @@ public class ChatGptSystemConfigRequest {
      * 非必填，默认值为TRUE
      */
     private Boolean restAiStream = Boolean.TRUE;
+
+    /**
+     * Get Azure OpenAI key credential from the Azure Portal
+     */
+    private String azureApiKey;
+
+    /**
+     * Get Azure OpenAI endpoint from the Azure Portal
+     */
+    private String azureEndpoint;
+
+    /**
+     * deployment id of the deployed model
+     */
+    private String azureDeploymentId;
 }
