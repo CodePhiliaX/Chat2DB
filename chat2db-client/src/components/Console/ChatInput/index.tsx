2b6d23bdcd586903767e9f29ee34a2308e6f9450
fanjin.fjy <fanjin.fjy@alibaba-inc.com>
2023-06-29 19:35:42 +0800

    feat: Optimize dashboard

--- chat2db-client/src/components/Console/ChatInput/index.tsx
+++ chat2db-client/src/components/Console/ChatInput/index.tsx
@@ -2,7 +2,7 @@ import React, { ChangeEvent, useEffect, useState } from 'react';
 import styles from './index.less';
 import AIImg from '@/assets/img/ai.svg';
 import { Input } from 'antd';
-import i18n from '@/i18n';
+import i18n from '@/i18n/';
 
 interface IProps {
   value?: string;
@@ -12,10 +12,20 @@ interface IProps {
 
 function ChatInput(props: IProps) {
   const onPressEnter = (e: any) => {
-    console.log('press enter', e.target.value);
     if (e.target.value) {
       props.onPressEnter && props.onPressEnter(e.target.value);
     }
+  };
+
+  const renderSuffix = () => {
+    const remainCnt = 10;
+    return (
+      <div className={styles.suffixBlock}>
+        <div className={styles.remainBlock}>
+          {i18n('chat.input.remain', remainCnt)}
+        </div>
+      </div>
+    );
   };
 
   return (
@@ -26,6 +36,7 @@ function ChatInput(props: IProps) {
         bordered={false}
         placeholder={i18n('workspace.ai.input.placeholder')}
         onPressEnter={onPressEnter}
+        suffix={renderSuffix()}
       />
     </div>
   );
