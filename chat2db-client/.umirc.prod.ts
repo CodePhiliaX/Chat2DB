1e513220a48d0460a247854a74eee9a0c2557209
shanhexi <wdbb8848@163.com>
2023-07-01 23:50:36 +0800

    fix:刷新白屏

--- chat2db-client/.umirc.prod.ts
+++ chat2db-client/.umirc.prod.ts
@@ -1,7 +1,7 @@
 import { formatDate } from './src/utils/date';
 import { defineConfig } from 'umi';
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
-const UMI_PublicPath = process.env.UMI_PublicPath || './';
+// const UMI_PublicPath = process.env.UMI_PublicPath || './static/front/';
 
 const chainWebpack = (config: any, { webpack }: any) => {
   config.plugin('monaco-editor').use(MonacoWebpackPlugin, [
@@ -12,7 +12,7 @@ const chainWebpack = (config: any, { webpack }: any) => {
 };
 
 export default defineConfig({
-  publicPath: UMI_PublicPath,
+  publicPath: './',
   chainWebpack,
   define: {
     'process.env.UMI_ENV': process.env.UMI_ENV,
