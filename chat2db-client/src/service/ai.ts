792414885e95f499e32af6352f5410f16b1871cf
Jerry Fan <huanyueyaoqin@qq.com>
2023-07-09 22:38:31 +0800

    feat: Replacement interface

--- chat2db-client/src/service/ai.ts
+++ chat2db-client/src/service/ai.ts
@@ -2,7 +2,7 @@ import { IChartItem, IDashboardItem, IPageResponse } from '@/typings';
 import { ILoginAndQrCode, IRemainingUse } from '@/typings/ai';
 import createRequest from './base';
 
-const getRemainingUse = createRequest<{ key: string }, IRemainingUse>('/api/client/remaininguses/:key', {
+const getRemainingUse = createRequest<{ key: string }, IRemainingUse>('/api/ai/config/remaininguses/:key', {
   errorLevel: false,
 });
 
