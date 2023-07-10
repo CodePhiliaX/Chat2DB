ea1c74e03923c26661567cd55cb6c2c3ffb03e2d
Jerry Fan <huanyueyaoqin@qq.com>
2023-07-10 20:55:37 +0800

    feat: add invite QrCode

--- chat2db-client/src/components/Popularize/index.tsx
+++ chat2db-client/src/components/Popularize/index.tsx
@@ -7,22 +7,30 @@ interface IProps {
   className?: string;
   source?: 'setting';
   imageUrl?: string;
+  tip?: string;
 }
 const url = 'https://oss-chat2db.alibaba.com/static/wechat.webp';
 export default memo<IProps>(function Popularize(props) {
-  const { className, source } = props;
+  const { className } = props;
+
+  const renderTip = () => {
+    if (props.tip) {
+      return <p>{props.tip}</p>;
+    }
+    let dom = '';
+    if (props.source === 'setting') {
+      dom += <p>{i18n('common.text.wechatPopularizeAi2')}</p>;
+    } else {
+      dom += <p>{i18n('common.text.wechatPopularizeAi')}</p>;
+    }
+    dom += <p>{i18n('common.text.wechatPopularize')}</p>;
+    return dom;
+  };
   return (
     <div className={classnames(styles.box, className)}>
       {/* <div className={styles.title}>获取更多次数</div> */}
       <img className={styles.wechatImg} src={props.imageUrl || url} />
-      <div className={styles.text}>
-        {source === 'setting' ? (
-          <p>{i18n('common.text.wechatPopularizeAi2')}</p>
-        ) : (
-          <p>{i18n('common.text.wechatPopularizeAi')}</p>
-        )}
-        <p>{i18n('common.text.wechatPopularize')}</p>
-      </div>
+      <div className={styles.text}>{renderTip()}</div>
     </div>
   );
 });
