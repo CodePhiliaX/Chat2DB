65549784ccffaac9aeeb88568eb82df13634fc1c
Jerry Fan <huanyueyaoqin@qq.com>
2023-07-21 21:44:45 +0800

    feat: Add google analysis

--- chat2db-client/.umirc.prod.ts
+++ chat2db-client/.umirc.prod.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'umi';
+import { extractYarnConfig } from './src/utils/webpack';
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+
 // const UMI_PublicPath = process.env.UMI_PublicPath || './static/front/';
+const yarn_config = extractYarnConfig(process.argv);
 
 const chainWebpack = (config: any, { webpack }: any) => {
   config.plugin('monaco-editor').use(MonacoWebpackPlugin, [
@@ -16,4 +19,15 @@ export default defineConfig({
   define: {
     'process.env.UMI_ENV': process.env.UMI_ENV,
   },
+  headScripts: [
+    `window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      window.dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+    gtag('config', 'G-V8M4E5SF61', {
+      platform: 'WEB',
+      version: '${yarn_config['app_version']}'
+    });`,
+  ],
 });
