156c577d4046ba6c68383da75b2d107684b75db7
JiaJu Zhuang <5152853@qq.com>
2023-07-23 16:13:10 +0800

    Add total number of fuzzy rows

--- chat2db-server/chat2db-spi/src/main/java/ai/chat2db/spi/model/ExecuteResult.java
+++ chat2db-server/chat2db-spi/src/main/java/ai/chat2db/spi/model/ExecuteResult.java
@@ -85,6 +85,12 @@ public class ExecuteResult {
     private Integer pageSize;
 
     /**
+     * Total number of fuzzy rows
+     * Only select statements have
+     */
+    private String fuzzyTotal;
+
+    /**
      * 执行持续时间
      */
     private Long duration;
