5c743e9fcb700edfa019c64ee493c02694edd987
fanjin.fjy <fanjin.fjy@alibaba-inc.com>
2023-07-29 18:08:16 +0800

    feat: Optimize code

--- chat2db-client/src/components/Popularize/index.tsx
+++ chat2db-client/src/components/Popularize/index.tsx
@@ -9,7 +9,8 @@ interface IProps {
   imageUrl?: string;
   tip?: string;
 }
-const url = 'https://oss-chat2db.alibaba.com/static/wechat.webp';
+const url =
+  'http://oss.sqlgpt.cn/static/chat2db-wechat.jpg?x-oss-process=image/auto-orient,1/resize,m_lfit,w_256/quality,Q_80/format,webp';
 export default memo<IProps>(function Popularize(props) {
   const { className } = props;
 
@@ -19,7 +20,7 @@ export default memo<IProps>(function Popularize(props) {
     }
     let dom;
     if (props.source === 'setting') {
-      dom = <p>{i18n('common.text.wechatPopularizeAi2')}</p>;
+      dom = <p>{'关注公众号获取AI Key'}</p>;
     } else {
       dom = <p>{i18n('common.text.wechatPopularizeAi')}</p>;
     }
---
+++ chat2db-client/src/typings/resultTable.ts
@@ -0,0 +1,10 @@
+import { IExecuteSqlParams } from '@/service/sql';
+
+export enum ExportTypeEnum {
+  CSV = 'CSV',
+  INSERT = 'INSERT',
+}
+export enum ExportSizeEnum {
+  CURRENT_PAGE = 'CURRENT_PAGE',
+  ALL = 'ALL',
+}
