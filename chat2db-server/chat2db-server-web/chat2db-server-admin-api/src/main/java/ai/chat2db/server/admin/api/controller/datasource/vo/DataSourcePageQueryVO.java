8dda9a34d2db3d31bf01f4877522ed76c7eadbb6
JiaJu Zhuang <5152853@qq.com>
2023-07-30 15:59:15 +0800

    Add Team Interface

---
+++ chat2db-server/chat2db-server-web/chat2db-server-admin-api/src/main/java/ai/chat2db/server/admin/api/controller/datasource/vo/DataSourcePageQueryVO.java
@@ -0,0 +1,28 @@
+
+package ai.chat2db.server.admin.api.controller.datasource.vo;
+
+import lombok.Data;
+
+/**
+ * Pagination query
+ *
+ * @author Jiaju Zhuang
+ */
+@Data
+public class DataSourcePageQueryVO {
+
+    /**
+     * 主键id
+     */
+    private Long id;
+
+    /**
+     * 连接别名
+     */
+    private String alias;
+
+    /**
+     * 连接地址
+     */
+    private String url;
+}
