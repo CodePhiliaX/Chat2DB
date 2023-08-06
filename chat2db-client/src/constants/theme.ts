b99f2a56a2c3d5e20ca4415d12a4b4bec69b5715
shanhexi <wdbb8848@163.com>
2023-08-06 16:12:41 +0800

    feat:添加主题色

--- chat2db-client/src/constants/theme.ts
+++ chat2db-client/src/constants/theme.ts
@@ -8,6 +8,11 @@ export enum PrimaryColorType {
   Polar_Green = 'polar-green',
   Golden_Purple = 'golden-purple',
   Polar_Blue = 'polar-blue',
+  Silver = 'silver',
+  Red = 'red',
+  Orange = 'orange',
+  Blue2 = 'blue2',
+  Gold = 'gold',
 }
 
 export enum LangType {
