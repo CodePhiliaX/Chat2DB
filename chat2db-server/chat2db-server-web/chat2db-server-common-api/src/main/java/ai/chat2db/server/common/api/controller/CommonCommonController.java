03d28f6b687f3b79c8c9d1fa1a714a034b6fe298
JiaJu Zhuang <5152853@qq.com>
2023-08-23 15:09:57 +0800

    Basic interface

--- chat2db-server/chat2db-server-web/chat2db-server-common-api/src/main/java/ai/chat2db/server/common/api/controller/CommonCommonController.java
+++ chat2db-server/chat2db-server-web/chat2db-server-common-api/src/main/java/ai/chat2db/server/common/api/controller/CommonCommonController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Some general data queries
+ * Basic interface
  *
  * @author Jiaju Zhuang
  */
