4629526b026ff86446c90a06420b9730638ad81a
SwallowGG <1558143046@qq.com>
2023-10-01 22:43:16 +0800

    support oracle table edit

--- chat2db-server/chat2db-plugins/chat2db-oracle/src/main/java/ai/chat2db/plugin/oracle/type/OracleIndexTypeEnum.java
+++ chat2db-server/chat2db-plugins/chat2db-oracle/src/main/java/ai/chat2db/plugin/oracle/type/OracleIndexTypeEnum.java
@@ -1,11 +1,11 @@
-package ai.chat2db.plugin.mysql.type;
+package ai.chat2db.plugin.oracle.type;
 
 import ai.chat2db.spi.enums.EditStatus;
 import ai.chat2db.spi.model.TableIndex;
 import ai.chat2db.spi.model.TableIndexColumn;
 import org.apache.commons.lang3.StringUtils;
 
-public enum MysqlIndexTypeEnum {
+public enum OracleIndexTypeEnum {
 
     PRIMARY_KEY("Primary", "PRIMARY KEY"),
 
@@ -13,9 +13,8 @@ public enum MysqlIndexTypeEnum {
 
     UNIQUE("Unique", "UNIQUE INDEX"),
 
-    FULLTEXT("Fulltext", "FULLTEXT INDEX"),
+    BITMAP("BITMAP", "BITMAP INDEX");
 
-    SPATIAL("Spatial", "SPATIAL INDEX");
 
     public String getName() {
         return name;
@@ -30,14 +29,14 @@ public enum MysqlIndexTypeEnum {
 
     private String keyword;
 
-    MysqlIndexTypeEnum(String name, String keyword) {
+    OracleIndexTypeEnum(String name, String keyword) {
         this.name = name;
         this.keyword = keyword;
     }
 
 
-    public static MysqlIndexTypeEnum getByType(String type) {
-        for (MysqlIndexTypeEnum value : MysqlIndexTypeEnum.values()) {
+    public static OracleIndexTypeEnum getByType(String type) {
+        for (OracleIndexTypeEnum value : OracleIndexTypeEnum.values()) {
             if (value.name.equalsIgnoreCase(type)) {
                 return value;
             }
@@ -47,33 +46,29 @@ public enum MysqlIndexTypeEnum {
 
     public String buildIndexScript(TableIndex tableIndex) {
         StringBuilder script = new StringBuilder();
-
-        script.append(keyword).append(" ");
-
-        script.append(buildIndexName(tableIndex)).append(" ");
-
-        script.append(buildIndexColumn(tableIndex)).append(" ");
-
-        script.append(buildIndexComment(tableIndex)).append(" ");
+        if (PRIMARY_KEY.equals(this)) {
+            script.append("CREATE PRIMARY KEY ");
+        } else if (UNIQUE.equals(this)) {
+            script.append("CREATE UNIQUE INDEX ");
+        } else {
+            script.append("CREATE INDEX ");
+        }
+        script.append(buildIndexName(tableIndex)).append(" ON \"").append(tableIndex.getTableName()).append("\" ").append(buildIndexColumn(tableIndex));
 
         return script.toString();
     }
 
-    private String buildIndexComment(TableIndex tableIndex) {
-        if(StringUtils.isBlank(tableIndex.getComment())){
-            return "";
-        }else {
-            return StringUtils.join("COMMENT '",tableIndex.getComment(),"'");
-        }
-
-    }
 
     private String buildIndexColumn(TableIndex tableIndex) {
         StringBuilder script = new StringBuilder();
         script.append("(");
         for (TableIndexColumn column : tableIndex.getColumnList()) {
-            if(StringUtils.isNotBlank(column.getColumnName())) {
-                script.append("`").append(column.getColumnName()).append("`").append(",");
+            if (StringUtils.isNotBlank(column.getColumnName())) {
+                script.append("\"").append(column.getColumnName()).append("\"");
+                if(!StringUtils.isBlank(column.getAscOrDesc())){
+                    script.append(" ").append(column.getAscOrDesc());
+                }
+                script .append(",");
             }
         }
         script.deleteCharAt(script.length() - 1);
@@ -82,11 +77,7 @@ public enum MysqlIndexTypeEnum {
     }
 
     private String buildIndexName(TableIndex tableIndex) {
-        if(this.equals(PRIMARY_KEY)){
-            return "";
-        }else {
-            return "`"+tableIndex.getName()+"`";
-        }
+        return "\"" + tableIndex.getSchemaName() + "\"" + "\"" + tableIndex.getName() + "\"";
     }
 
     public String buildModifyIndex(TableIndex tableIndex) {
@@ -94,18 +85,22 @@ public enum MysqlIndexTypeEnum {
             return buildDropIndex(tableIndex);
         }
         if (EditStatus.MODIFY.name().equals(tableIndex.getEditStatus())) {
-            return StringUtils.join(buildDropIndex(tableIndex),",\n", "ADD ", buildIndexScript(tableIndex));
+            return StringUtils.join(buildDropIndex(tableIndex), ",\n" , buildIndexScript(tableIndex));
         }
         if (EditStatus.ADD.name().equals(tableIndex.getEditStatus())) {
-            return StringUtils.join("ADD ", buildIndexScript(tableIndex));
+            return StringUtils.join( buildIndexScript(tableIndex));
         }
         return "";
     }
 
     private String buildDropIndex(TableIndex tableIndex) {
-        if (MysqlIndexTypeEnum.PRIMARY_KEY.getName().equals(tableIndex.getType())) {
+        if (OracleIndexTypeEnum.PRIMARY_KEY.getName().equals(tableIndex.getType())) {
             return StringUtils.join("DROP PRIMARY KEY");
         }
-        return StringUtils.join("DROP INDEX `", tableIndex.getOldName(),"`");
+        StringBuilder script = new StringBuilder();
+        script.append("DROP INDEX ");
+        script.append(buildIndexName(tableIndex));
+
+        return script.toString();
     }
 }
