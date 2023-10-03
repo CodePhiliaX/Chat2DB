2dabb8eb39b3368c199fa8e1dc546e3993bc8971
robin <850379744@qq.com>
2023-10-03 16:47:50 +0800

    add common ai support

--- chat2db-server/chat2db-server-web/chat2db-server-web-api/src/main/java/ai/chat2db/server/web/api/controller/ai/fastchat/interceptor/FastChatHeaderAuthorizationInterceptor.java
+++ chat2db-server/chat2db-server-web/chat2db-server-web-api/src/main/java/ai/chat2db/server/web/api/controller/ai/fastchat/interceptor/FastChatHeaderAuthorizationInterceptor.java
@@ -1,7 +1,9 @@
 package ai.chat2db.server.web.api.controller.ai.fastchat.interceptor;
 
+import cn.hutool.core.util.RandomUtil;
 import cn.hutool.http.ContentType;
 import cn.hutool.http.Header;
+import com.google.common.collect.Lists;
 import lombok.Getter;
 import okhttp3.Interceptor;
 import okhttp3.Request;
@@ -28,7 +30,8 @@ public class FastChatHeaderAuthorizationInterceptor implements Interceptor {
     public Response intercept(Chain chain) throws IOException {
         Request original = chain.request();
         Request request = original.newBuilder()
-                .header("apiKey", apiKey) // replace to your corresponding field and value
+                // replace to your corresponding field and value
+                .header(Header.AUTHORIZATION.getValue(), "Bearer " + apiKey)
                 .header(Header.CONTENT_TYPE.getValue(), ContentType.JSON.getValue())
                 .method(original.method(), original.body())
                 .build();
