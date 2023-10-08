d37a0507f585741c6be07fcca7abe15ec52922ec
shanhexi <wdbb8848@163.com>
2023-10-08 17:04:42 +0800

    edit data

--- chat2db-client/src/service/sql.ts
+++ chat2db-client/src/service/sql.ts
@@ -208,9 +208,13 @@ export interface IModifyTableSqlParams {
 const getModifyTableSql = createRequest<IModifyTableSqlParams, { sql: string }[]>('/api/rdb/table/modify/sql', { method: 'post' });
 
 /** 执行编辑表的sql, 专为编辑表而生 */ 
-const executeDDL = createRequest<IExecuteSqlParams, {success: boolean, message: string}>('/api/rdb/dml/execute_ddl', {method:'post'});
+const executeDDL = createRequest<IExecuteSqlParams, { success: boolean, message: string }>('/api/rdb/dml/execute_ddl', { method: 'post' });
+
+/** 获取修改表数据的接口 */ 
+const getExecuteUpdateSql = createRequest<any, { sql: string }[]>('/api/rdb/dml/execute_update', { method: 'post' });
 
 export default {
+  getExecuteUpdateSql,
   executeDDL,
   getModifyTableSql,
   getTableDetails,
