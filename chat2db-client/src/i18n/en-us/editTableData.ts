126442e35e9b90531b07099efbd584920a1f1f9c
shanhexi <wdbb8848@163.com>
2023-10-11 14:44:20 +0800

    feat: edit table data tips

---
+++ chat2db-client/src/i18n/en-us/editTableData.ts
@@ -0,0 +1,7 @@
+export default {
+  'editTableData.tips.addRow': 'Add Row',
+  'editTableData.tips.deleteRow': 'Delete Row',
+  'editTableData.tips.revert': 'Revert',
+  'editTableData.tips.previewPendingChanges': 'Preview Pending Changes',
+  'editTableData.tips.submit': 'Submit',
+};
