6834e38c7f17047ffa559e474504187a310a1dee
Jerry Fan <huanyueyaoqin@qq.com>
2023-10-12 15:37:04 +0800

    feat: Supplement keywords and functions from different databases

--- chat2db-client/src/constants/IntelliSense/index.ts
+++ chat2db-client/src/constants/IntelliSense/index.ts
@@ -1,4 +1,6 @@
 import mysql from './mysql';
 import oracle from './oracle';
+import postgresql  from './pgsql';
+import redis from './redis';
 
-export default { mysql, oracle };
+export default { mysql, oracle, postgresql, redis };
