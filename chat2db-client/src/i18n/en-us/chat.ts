85e4b62c090d90a580ffce71c4538fa3c22ade1a
Jerry Fan <huanyueyaoqin@qq.com>
2023-10-13 20:51:50 +0800

    feat: Add tips about auto sync table to AI

--- chat2db-client/src/i18n/en-us/chat.ts
+++ chat2db-client/src/i18n/en-us/chat.ts
@@ -1,9 +1,9 @@
 export default {
   'chat.input.remain': '{1} remaining',
-  'chat.input.remain.tooltip': 'Table schema added to prompt.',
   'chat.input.tableSelect.placeholder':'Please choose tables',
   'chat.input.tableSelect.error.TooManyTable':'You can only select up to 8 tables',
   'chat.input.remain.dialog.tips':
-    'Subscribe our official WeChat account, send 推广 to get more chances to experience.',
-
+  'Subscribe our official WeChat account, send 推广 to get more chances to experience.',
+  "chat.input.syncTable.tips":'Mode One: Automatically synchronize all tables to the AI context',
+  'chat.input.remain.tooltip': 'Mode Two: Table schema added to prompt.',
 };
--- chat2db-client/src/i18n/zh-cn/chat.ts
+++ chat2db-client/src/i18n/zh-cn/chat.ts
@@ -1,9 +1,8 @@
 export default {
   'chat.input.remain': '剩余 {1} 次',
-  'chat.input.remain.tooltip': '选中表的schema将会被添加到prompt中',
   'chat.input.tableSelect.placeholder': '请选择表',
   'chat.input.tableSelect.error.TooManyTable': '最多选择8张表',
-  'chat.input.remain.dialog.tips':
-    '关注公众号，发送"推广"获取更多体验次数',
-
+  'chat.input.remain.dialog.tips': '关注公众号，发送"推广"获取更多体验次数',
+  'chat.input.syncTable.tips': '模式一: 自动同步所有表给AI上下文',
+  'chat.input.remain.tooltip': '模式二: 选中表的schema将会被添加到prompt中',
 };
