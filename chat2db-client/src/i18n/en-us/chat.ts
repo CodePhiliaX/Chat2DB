5487c23cf7bef986616e95ddda07f2496d8d990a
Jerry Fan <huanyueyaoqin@qq.com>
2023-10-13 21:35:18 +0800

    fix: Optimize sync table

--- chat2db-client/src/i18n/en-us/chat.ts
+++ chat2db-client/src/i18n/en-us/chat.ts
@@ -1,9 +1,10 @@
 export default {
   'chat.input.remain': '{1} remaining',
-  'chat.input.tableSelect.placeholder':'Please choose tables',
-  'chat.input.tableSelect.error.TooManyTable':'You can only select up to 8 tables',
+  'chat.input.tableSelect.placeholder': 'Please choose tables',
+  'chat.input.tableSelect.error.TooManyTable': 'You can only select up to 8 tables',
   'chat.input.remain.dialog.tips':
-  'Subscribe our official WeChat account, send 推广 to get more chances to experience.',
-  "chat.input.syncTable.tips":'Mode One: Automatically synchronize all tables to the AI context',
-  'chat.input.remain.tooltip': 'Mode Two: Table schema added to prompt.',
+    'Subscribe our official WeChat account, send 推广 to get more chances to experience.',
+  'chat.input.syncTable.tips': 'The automatically synchronize all table structures to the AI context',
+  'chat.input.remain.tooltip': 'The manually selected table will be synchronized to the AI context',
+  'chat.input.syncTable.tempTips': '🎉Update: Automatically synchronize all table structures to the AI context',
 };
