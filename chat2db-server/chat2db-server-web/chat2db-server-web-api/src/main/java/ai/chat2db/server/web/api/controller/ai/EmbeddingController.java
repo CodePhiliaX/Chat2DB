07638b6211f81bfd572103565d6fac735c735f66
robin <850379744@qq.com>
2023-10-22 20:43:13 +0800

    vector update

--- chat2db-server/chat2db-server-web/chat2db-server-web-api/src/main/java/ai/chat2db/server/web/api/controller/ai/EmbeddingController.java
+++ chat2db-server/chat2db-server-web/chat2db-server-web-api/src/main/java/ai/chat2db/server/web/api/controller/ai/EmbeddingController.java
@@ -21,6 +21,7 @@ import ai.chat2db.server.web.api.controller.rdb.converter.RdbWebConverter;
 import ai.chat2db.server.web.api.controller.rdb.request.TableBriefQueryRequest;
 import ai.chat2db.server.web.api.controller.rdb.request.TableMilvusQueryRequest;
 import ai.chat2db.server.web.api.http.GatewayClientService;
+import ai.chat2db.server.web.api.http.request.EsTableSchemaRequest;
 import ai.chat2db.server.web.api.http.request.TableSchemaRequest;
 import ai.chat2db.server.web.api.http.request.WhiteListRequest;
 import ai.chat2db.server.web.api.util.ApplicationContextUtil;
@@ -148,6 +149,81 @@ public class EmbeddingController extends ChatController {
     }
 
     /**
+     * save datasource schema
+     *
+     * @param request
+     * @return
+     * @throws IOException
+     */
+    @PostMapping("/datasource/es")
+    @CrossOrigin
+    public ActionResult es(@Valid EsTableSchemaRequest request)
+            throws Exception {
+
+        // query tables
+        TablePageQueryParam queryParam = rdbWebConverter.schemaReq2page(request);
+        TableSelector tableSelector = new TableSelector();
+        tableSelector.setColumnList(false);
+        tableSelector.setIndexList(false);
+        queryParam.setPageNo(1);
+        queryParam.setPageSize(1000);
+        PageResult<Table> tableDTOPageResult = tableService.pageQuery(queryParam, tableSelector);
+
+        List<Table> tables = tableDTOPageResult.getData();
+        if (CollectionUtils.isEmpty(tables)) {
+            return ActionResult.isSuccess();
+        }
+        String tableName = tables.get(0).getName();
+        String tableSchema = queryTableDdlByEs(tableName, request);
+        request.setTableName(tableName);
+        request.setTableSchemaContent(tableSchema);
+
+        if (StringUtils.isBlank(tableSchema)) {
+            throw new ParamBusinessException("tableSchema is empty");
+        }
+
+        // save first table embedding
+        request.setDeleteBeforeInsert(true);
+        saveTableEs(request);
+
+        // save other table embedding
+        request.setDeleteBeforeInsert(false);
+        for (int i = 1; i < tables.size(); i++) {
+            tableName = tables.get(i).getName();
+            tableSchema = queryTableDdlByEs(tableName, request);
+            if (StringUtils.isBlank(tableSchema)) {
+                continue;
+            }
+            request.setTableName(tableName);
+            request.setTableSchemaContent(tableSchema);
+            saveTableEs(request);
+        }
+
+        // query all the tables
+        Long totalTableCount = tableDTOPageResult.getTotal();
+        Integer pageSize = queryParam.getPageSize();
+        if (pageSize < totalTableCount) {
+            for (int i = 2; i < totalTableCount/pageSize + 1; i++) {
+                queryParam.setPageNo(i);
+                tableDTOPageResult = tableService.pageQuery(queryParam, tableSelector);
+                tables = tableDTOPageResult.getData();
+                for (Table table : tables) {
+                    tableName = table.getName();
+                    tableSchema = queryTableDdlByEs(tableName, request);
+                    if (StringUtils.isBlank(tableSchema)) {
+                        continue;
+                    }
+                    request.setTableName(tableName);
+                    request.setTableSchemaContent(tableSchema);
+                    saveTableEs(request);
+                }
+            }
+        }
+
+        return ActionResult.isSuccess();
+    }
+
+    /**
      * sync table vector
      *
      * @param param
@@ -224,6 +300,56 @@ public class EmbeddingController extends ChatController {
     }
 
     /**
+     * sync table vector
+     *
+     * @param param
+     */
+    public void syncTableEs(TableBriefQueryRequest param) throws Exception {
+        EsTableSchemaRequest esParam = rdbWebConverter.req2req(param);
+        if (Objects.isNull(esParam.getDataSourceId())) {
+            return;
+        }
+        if (StringUtils.isBlank(esParam.getDatabaseName()) && StringUtils.isBlank(esParam.getSchemaName())) {
+            return;
+        }
+
+        ConfigService configService = ApplicationContextUtil.getBean(ConfigService.class);
+        Config config = configService.find(RestAIClient.AI_SQL_SOURCE).getData();
+        String aiSqlSource = AiSqlSourceEnum.CHAT2DBAI.getCode();
+        // only sync for chat2db ai
+        if (Objects.isNull(config) || !aiSqlSource.equals(config.getContent())) {
+            return;
+        }
+        Config keyConfig = configService.find(Chat2dbAIClient.CHAT2DB_OPENAI_KEY).getData();
+        if (Objects.isNull(keyConfig) || StringUtils.isBlank(keyConfig.getContent())) {
+            return;
+        }
+
+        String apiKey = keyConfig.getContent();
+        TableVectorParam vectorParam = rdbWebConverter.param2param(param);
+        vectorParam.setApiKey(apiKey);
+        DataResult<Boolean> result = tableService.checkTableVector(vectorParam);
+        if (result.getData()) {
+            return;
+        }
+
+        esParam.setApiKey(apiKey);
+        es(esParam);
+        tableService.saveTableVector(vectorParam);
+    }
+
+    /**
+     * save table schema
+     *
+     * @param tableSchemaRequest
+     * @throws Exception
+     */
+    private void saveTableEs(EsTableSchemaRequest tableSchemaRequest) throws Exception{
+        // save table es
+        gatewayClientService.schemaEsSave(tableSchemaRequest);
+    }
+
+    /**
      * query table schema
      *
      * @param tableName
@@ -231,6 +357,23 @@ public class EmbeddingController extends ChatController {
      * @return
      */
     private String queryTableDdl(String tableName, TableBriefQueryRequest request) {
+        ShowCreateTableParam param = new ShowCreateTableParam();
+        param.setTableName(tableName);
+        param.setDataSourceId(request.getDataSourceId());
+        param.setDatabaseName(request.getDatabaseName());
+        param.setSchemaName(request.getSchemaName());
+        DataResult<String> tableSchema = tableService.showCreateTable(param);
+        return tableSchema.getData();
+    }
+
+    /**
+     * query table schema
+     *
+     * @param tableName
+     * @param request
+     * @return
+     */
+    private String queryTableDdlByEs(String tableName, EsTableSchemaRequest request) {
         ShowCreateTableParam param = new ShowCreateTableParam();
         param.setTableName(tableName);
         param.setDataSourceId(request.getDataSourceId());
