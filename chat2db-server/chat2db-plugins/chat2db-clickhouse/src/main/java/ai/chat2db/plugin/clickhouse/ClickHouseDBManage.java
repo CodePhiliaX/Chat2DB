867467ca597df48ccb115bf5836fd60d6bac4ca7
ljc <jiacheng.li@lenkor.cn>
2023-10-27 11:37:43 +0800

    fix: 修复clickhouse无法选中数据库问题

--- chat2db-server/chat2db-plugins/chat2db-clickhouse/src/main/java/ai/chat2db/plugin/clickhouse/ClickHouseDBManage.java
+++ chat2db-server/chat2db-plugins/chat2db-clickhouse/src/main/java/ai/chat2db/plugin/clickhouse/ClickHouseDBManage.java
@@ -4,10 +4,39 @@ import java.sql.Connection;
 
 import ai.chat2db.spi.DBManage;
 import ai.chat2db.spi.jdbc.DefaultDBManage;
+import ai.chat2db.spi.sql.ConnectInfo;
 import ai.chat2db.spi.sql.SQLExecutor;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.net.ssl.HostnameVerifier;
 
 public class ClickHouseDBManage extends DefaultDBManage implements DBManage {
 
+    @Override
+    public Connection getConnection(ConnectInfo connectInfo) {
+        String url = setDatabaseInJdbcUrl(connectInfo);
+        connectInfo.setUrl(url);
+
+        return super.getConnection(connectInfo);
+    }
+
+    private String setDatabaseInJdbcUrl(ConnectInfo connectInfo) {
+        String databaseName;
+        String url = connectInfo.getUrl();
+        if (StringUtils.isBlank((databaseName = connectInfo.getDatabaseName())) && StringUtils.isBlank((databaseName = connectInfo.getSchemaName()))) {
+            return url;
+        }
+
+        String connectAddress = connectInfo.getHost() + ":" + connectInfo.getPort();
+        String[] addressSplit = url.split(connectAddress);
+        String connectParams = addressSplit[1];
+        if (connectParams.startsWith("/")) {
+            // 删除连接参数中的 /
+            connectParams = connectParams.substring(1);
+        }
+        // 添加数据库名
+        return addressSplit[0] + connectAddress + "/" + databaseName + connectParams;
+    }
 
     @Override
     public void dropTable(Connection connection, String databaseName, String schemaName, String tableName) {
