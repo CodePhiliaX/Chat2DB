654462ad1c6e5bfe2633959ebd165de71b92cad5
SwallowGG <1558143046@qq.com>
2023-11-12 12:26:48 +0800

    Fix kingbase8r6 error

--- chat2db-server/chat2db-spi/src/main/java/ai/chat2db/spi/util/ResultSetUtils.java
+++ chat2db-server/chat2db-spi/src/main/java/ai/chat2db/spi/util/ResultSetUtils.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Map;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.google.common.collect.Lists;
@@ -55,6 +56,7 @@ public class ResultSetUtils {
             ObjectMapper mapper = new ObjectMapper();
             mapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
             mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            mapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
             while (rs.next()) {
                 Map<String, Object> map = new HashMap<>();
                 for (int i = 1; i <= col; i++) {
