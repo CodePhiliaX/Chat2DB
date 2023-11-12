ff4b0e1211a8cd8b7502ab8a801a8ea1121406f1
SwallowGG <1558143046@qq.com>
2023-11-12 12:57:23 +0800

    Fix kingbase8r6 error

--- chat2db-server/chat2db-plugins/chat2db-kingbase/src/main/java/ai/chat2db/plugin/kingbase/builder/DBConfigBuilder.java
+++ chat2db-server/chat2db-plugins/chat2db-kingbase/src/main/java/ai/chat2db/plugin/kingbase/builder/DBConfigBuilder.java
@@ -9,12 +9,20 @@ public class DBConfigBuilder {
         DBConfig dbConfig = new DBConfig();
         dbConfig.setName("KingBase");
         dbConfig.setDbType("KINGBASE");
+
+
         DriverConfig driverConfig = new DriverConfig();
         driverConfig.setJdbcDriver("kingbase8-8.6.0.jar");
         driverConfig.setJdbcDriverClass("com.kingbase8.Driver");
         driverConfig.setDownloadJdbcDriverUrls(Lists.newArrayList("https://oss.sqlgpt.cn/lib/kingbase8-8.6.0.jar"));
         dbConfig.setDefaultDriverConfig(driverConfig);
-        dbConfig.setDriverConfigList(Lists.newArrayList(driverConfig));
+
+        DriverConfig driverConfig1 = new DriverConfig();
+        driverConfig1.setJdbcDriver("kingbase8-8.2.0.jar");
+        driverConfig1.setJdbcDriverClass("com.kingbase8.Driver");
+        driverConfig1.setDownloadJdbcDriverUrls(Lists.newArrayList("https://oss.sqlgpt.cn/lib/kingbase8-8.2.0.jar"));
+
+        dbConfig.setDriverConfigList(Lists.newArrayList(driverConfig,driverConfig1));
         return dbConfig;
     }
 }
