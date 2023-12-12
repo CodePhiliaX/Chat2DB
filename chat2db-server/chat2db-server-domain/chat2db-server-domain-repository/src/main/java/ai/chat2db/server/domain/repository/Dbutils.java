b9606007d235dd8afefee16658af05f70668d251
SwallowGG <1558143046@qq.com>
2023-12-12 23:22:20 +0800

    Improve startup speed

--- chat2db-server/chat2db-server-domain/chat2db-server-domain-repository/src/main/java/ai/chat2db/server/domain/repository/Dbutils.java
+++ chat2db-server/chat2db-server-domain/chat2db-server-domain-repository/src/main/java/ai/chat2db/server/domain/repository/Dbutils.java
@@ -146,7 +146,7 @@ public class Dbutils {
         String environment = StringUtils.defaultString(System.getProperty("spring.profiles.active"), "dev");
         if ("dev".equalsIgnoreCase(environment)) {
             dataSource.setJdbcUrl("jdbc:h2:file:~/.chat2db/db/chat2db_dev;MODE=MYSQL");
-        } if ("test".equalsIgnoreCase(environment)) {
+        }else if ("test".equalsIgnoreCase(environment)) {
             dataSource.setJdbcUrl("jdbc:h2:file:~/.chat2db/db/chat2db_test;MODE=MYSQL");
         }else {
             dataSource.setJdbcUrl("jdbc:h2:~/.chat2db/db/chat2db;MODE=MYSQL;FILE_LOCK=NO");
