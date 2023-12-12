1ae7016d024ec51b5ba5f3a720be77ce1d649990
SwallowGG <1558143046@qq.com>
2023-12-12 20:06:51 +0800

    Improve startup speed

--- chat2db-server/chat2db-server-web-start/src/main/resources/db/migration/V2_1_6__TableVectorUpdate.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE table_vector_mapping ALTER COLUMN status VARCHAR(64);
--- chat2db-server/chat2db-server-web-start/src/main/resources/db/migration/V2_1_7__DATASOURCE.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `data_source` ADD COLUMN `service_name` varchar(128) NULL COMMENT '服务名';
-ALTER TABLE `data_source` ADD COLUMN `service_type` varchar(128) NULL COMMENT '服务类型';
-
