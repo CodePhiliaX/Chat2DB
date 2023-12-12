b07ec45de434030acea10604d753cdecf6583d26
shanhexi <wdbb8848@163.com>
2023-12-12 22:59:22 +0800

    delete:holdingService

--- chat2db-client/src/main/index.js
+++ chat2db-client/src/main/index.js
@@ -79,7 +79,7 @@ app.on('before-quit', () => {
           'Content-Type': 'application/json',
         },
         method: 'POST',
-        url: `${baseUrl}/api/system/stop?forceQuit=${_forceQuitCode}`,
+        url: `${baseUrl}/api/system/stop?forceQuit=true`,
       });
       request.end();
     } catch (error) {}
