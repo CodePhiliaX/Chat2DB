90037b7cbe3f832f138be175ba78ca0b03de38dc
Samet UCA <sametuca@hotmail.com>
2023-12-18 19:32:45 +0300

    added turkish language

---
+++ chat2db-client/src/i18n/tr-tr/chat.ts
@@ -0,0 +1,10 @@
+export default {
+  'chat.input.remain': 'Kalan {1}',
+  'chat.input.tableSelect.placeholder': 'Lütfen tabloları seçin',
+  'chat.input.tableSelect.error.TooManyTable': 'En fazla 8 tablo seçebilirsiniz',
+  'chat.input.remain.dialog.tips':
+    'Resmi WeChat hesabımıza abone olun, daha fazla deneyim şansı için 推广 gönderin.',
+  'chat.input.syncTable.tips': 'Otomatik olarak tüm tablo yapılarını AI bağlamına senkronize eder',
+  'chat.input.remain.tooltip': 'Manuel olarak seçilen tablo, AI bağlamına senkronize edilecektir',
+  'chat.input.syncTable.tempTips': '🎉Güncelleme: Otomatik olarak tüm tablo yapılarını AI bağlamına senkronize etme',
+};
---
+++ chat2db-client/src/i18n/tr-tr/sqlEditor.ts
@@ -0,0 +1,9 @@
+export default {
+  'sqlEditor.text.keyword': 'Anahtar Kelime',
+  'sqlEditor.text.function': 'Fonksiyon',
+  'sqlEditor.text.tableName': 'Tablo Adı',
+  'sqlEditor.text.databaseName': 'Veritabanı Adı',
+  'sqlEditor.text.schemaName': 'Şema',
+  'sqlEditor.text.viewName': 'Görünüm Adı',
+  'sqlEditor.text.fieldName': 'Alan Adı',
+};
