7690575e6b12fe21052e8a58f4ef00809003bf39
leven.chen <leven.chen@dji.com>
2023-12-22 17:58:05 +0800

    feature: dify chat stream client add builder

--- chat2db-server/chat2db-server-web/chat2db-server-web-api/src/main/java/ai/chat2db/server/web/api/controller/ai/dify/client/DifyChatAiStreamClient.java
+++ chat2db-server/chat2db-server-web/chat2db-server-web-api/src/main/java/ai/chat2db/server/web/api/controller/ai/dify/client/DifyChatAiStreamClient.java
@@ -1,4 +1,30 @@
 package ai.chat2db.server.web.api.controller.ai.dify.client;
 
+import ai.chat2db.server.web.api.controller.ai.dify.listener.DifyChatAIEventSourceListener;
+import com.unfbx.chatgpt.entity.chat.Message;
+import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
 public class DifyChatAiStreamClient {
+
+    /**
+     * apikey
+     */
+    @Getter
+    @NotNull
+    private String apiKey;
+
+    /**
+     * apiHost
+     */
+    @Getter
+    @NotNull
+    private String apiHost;
+
+
+    public void streamCompletions(List<Message> messages, DifyChatAIEventSourceListener eventSourceListener) {
+
+    }
 }
