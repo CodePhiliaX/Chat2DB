d933c1c8d467bd89f1a7d833459bfe6196bc192d
shanhexi <wdbb8848@163.com>
2024-01-02 20:36:54 +0800

    delete:fingerprintjs2

--- chat2db-client/src/pages/demo/index.tsx
+++ chat2db-client/src/pages/demo/index.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
-// import styles from './index.less';
-import Fingerprint2 from 'fingerprintjs2';
 
 function Test() {
-  Fingerprint2.get((com)=>{
-    console.log(com);
-  })
   return (false);
 }
 
