9c07cd49806ffb62f5411ecb2d251d66d07eda9b
shanhexi <wdbb8848@163.com>
2024-01-08 13:10:19 +0800

    feat:electron index.js build

--- chat2db-client/src/main/webpack.config.js
+++ chat2db-client/src/main/webpack.config.js
@@ -3,7 +3,8 @@ const path = require('path');
 module.exports = {
   entry: './index.js',
   output: {
-    filename: 'main.js',
+    filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  target: 'node'
 };
