1ed380fe052ad95227f02f7effec900464798794
shanhexi <wdbb8848@163.com>
2024-01-09 17:34:55 +0800

    iconfont

--- chat2db-client/src/components/Iconfont/index.tsx
+++ chat2db-client/src/components/Iconfont/index.tsx
@@ -9,9 +9,9 @@ if (__ENV__ === 'local') {
   /* 在线链接服务仅供平台体验和调试使用，平台不承诺服务的稳定性，企业客户需下载字体包自行发布使用并做好备份。 */
   @font-face {
     font-family: 'iconfont';  /* Project id 3633546 */
-    src: url('//at.alicdn.com/t/c/font_3633546_wtvchggsz6n.woff2?t=1704785517485') format('woff2'),
-         url('//at.alicdn.com/t/c/font_3633546_wtvchggsz6n.woff?t=1704785517485') format('woff'),
-         url('//at.alicdn.com/t/c/font_3633546_wtvchggsz6n.ttf?t=1704785517485') format('truetype');
+    src: url('//at.alicdn.com/t/c/font_3633546_n19od3mt6mo.woff2?t=1704792255884') format('woff2'),
+         url('//at.alicdn.com/t/c/font_3633546_n19od3mt6mo.woff?t=1704792255884') format('woff'),
+         url('//at.alicdn.com/t/c/font_3633546_n19od3mt6mo.ttf?t=1704792255884') format('truetype');
   }
   `;
   const style = document.createElement('style');
