4f0064912d9d2fab5ae1f230103d6e41d8601333
tmlx1990 <tmlx1990@163.com>
2024-02-28 17:23:27 +0800

    fix：1186 修复MySQL不能获取表注释的问题。 增加useInformationSchema属性为true。

--- chat2db-server/chat2db-plugins/chat2db-mysql/src/main/java/ai/chat2db/plugin/mysql/MysqlDBManage.java
+++ chat2db-server/chat2db-plugins/chat2db-mysql/src/main/java/ai/chat2db/plugin/mysql/MysqlDBManage.java
@@ -1,12 +1,15 @@
 package ai.chat2db.plugin.mysql;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-
 import ai.chat2db.spi.DBManage;
 import ai.chat2db.spi.jdbc.DefaultDBManage;
+import ai.chat2db.spi.model.KeyValue;
+import ai.chat2db.spi.sql.ConnectInfo;
 import ai.chat2db.spi.sql.SQLExecutor;
 import org.springframework.util.StringUtils;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
 
 public class MysqlDBManage extends DefaultDBManage implements DBManage {
     @Override
@@ -27,6 +30,17 @@ public class MysqlDBManage extends DefaultDBManage implements DBManage {
     public void dropTable(Connection connection, String databaseName, String schemaName, String tableName) {
         String sql = "DROP TABLE "+ format(tableName);
         SQLExecutor.getInstance().execute(connection,sql, resultSet -> null);
+    }
+
+    @Override
+    public Connection getConnection(ConnectInfo connectInfo) {
+        KeyValue keyValue = new KeyValue();
+        keyValue.setKey("useInformationSchema");
+        keyValue.setValue("true");
+        List<KeyValue> extendInfoList = connectInfo.getExtendInfo();
+        extendInfoList.add(keyValue);
+        connectInfo.setExtendInfo(extendInfoList);
+        return super.getConnection(connectInfo);
     }
 
     public static String format(String tableName) {
