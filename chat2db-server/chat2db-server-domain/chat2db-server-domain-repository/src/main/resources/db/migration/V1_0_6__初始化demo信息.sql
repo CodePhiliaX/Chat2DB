d9c7d7f78b72bcc3efb9bb04eaa7ac47306c96d3
hantianwei <hantianwei@gmail.com>
2024-03-07 23:39:25 +0800

    Adaptation deployment data source: MYSQL - Modify the initialization script - ssl Indicates `ssl`  to match the mysql keyword - schema is changed to `schema`  to match the mysql keyword - Change bigint(20) to bigint: Integer display width is deprecated and will be removed in a future release. - MODIFY the field length. Use: Replace ALTER with modify to adapt MYSQL - Modify the DO mysql keyword processing - schema Added @TableField(" `schema` ") annotation - ssl Added @TableField(" `ssl` ") annotation

--- chat2db-server/chat2db-server-domain/chat2db-server-domain-repository/src/main/resources/db/migration/V1_0_6__初始化demo信息.sql
+++ chat2db-server/chat2db-server-domain/chat2db-server-domain-repository/src/main/resources/db/migration/V1_0_6__初始化demo信息.sql
@@ -1,16 +1,16 @@
 INSERT INTO DATA_SOURCE (GMT_CREATE, GMT_MODIFIED, ALIAS, URL, USER_NAME, PASSWORD, TYPE, USER_ID, HOST, PORT, SSH,JDBC)
 VALUES (CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'DEMO@db.sqlgpt.cn', 'jdbc:mysql://db.sqlgpt.cn:3306/DEMO', 'demo', 'kok39AYoOSM=', 'MYSQL', 0, 'db.sqlgpt.cn', '3306', '{"use":false}', '8.0');
 
-INSERT INTO DASHBOARD (ID, GMT_CREATE, GMT_MODIFIED, NAME, DESCRIPTION, SCHEMA, DELETED, USER_ID)
+INSERT INTO DASHBOARD (ID, GMT_CREATE, GMT_MODIFIED, NAME, DESCRIPTION, `SCHEMA`, DELETED, USER_ID)
 VALUES (1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '学生成绩分析', '学生成绩分析', '[[1],[2],[3]]', 'N', 0);
 
-INSERT INTO CHART (ID, GMT_CREATE, GMT_MODIFIED,  SCHEMA, DATA_SOURCE_ID, DATABASE_NAME, DDL, DELETED, USER_ID)
+INSERT INTO CHART (ID, GMT_CREATE, GMT_MODIFIED,  `SCHEMA`, DATA_SOURCE_ID, DATABASE_NAME, DDL, DELETED, USER_ID)
 VALUES (1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP,  '{"chartType":"Column","xAxis":"name","yAxis":"total_score"}', 1, 'DEMO', 'SELECT s.name, sc.chinese_score, sc.math_score, sc.english_score, sc.science_score, sc.humanities_score,
 (sc.chinese_score + sc.math_score + sc.english_score + sc.science_score + sc.humanities_score) AS total_score
 FROM student s
 JOIN score sc ON s.id = sc.student_id', 'N', 0);
 
-INSERT INTO CHART (ID, GMT_CREATE, GMT_MODIFIED,  SCHEMA, DATA_SOURCE_ID, DATABASE_NAME, DDL, DELETED, USER_ID)
+INSERT INTO CHART (ID, GMT_CREATE, GMT_MODIFIED,  `SCHEMA`, DATA_SOURCE_ID, DATABASE_NAME, DDL, DELETED, USER_ID)
 VALUES (2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '{"chartType":"Pie","xAxis":"grade"}', 1, 'DEMO', 'SELECT s.name,
        score.chinese_score,
        score.math_score,
@@ -27,7 +27,7 @@ VALUES (2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '{"chartType":"Pie","xAxis":"gr
 FROM score
 JOIN student s ON score.student_id = s.id', 'N', 0);
 
-INSERT INTO CHART (ID, GMT_CREATE, GMT_MODIFIED,  SCHEMA, DATA_SOURCE_ID, DATABASE_NAME, DDL, DELETED, USER_ID)
+INSERT INTO CHART (ID, GMT_CREATE, GMT_MODIFIED,  `SCHEMA`, DATA_SOURCE_ID, DATABASE_NAME, DDL, DELETED, USER_ID)
 VALUES (3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP,  '{"chartType":"Line","xAxis":"name","yAxis":"chinese_score"}', 1, 'DEMO', 'SELECT s.name, sc.chinese_score, sc.math_score, sc.english_score, sc.science_score, sc.humanities_score,
 (sc.chinese_score + sc.math_score + sc.english_score + sc.science_score + sc.humanities_score) AS total_score
 FROM student s
