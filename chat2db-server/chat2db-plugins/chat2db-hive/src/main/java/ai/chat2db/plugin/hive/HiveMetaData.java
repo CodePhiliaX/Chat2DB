52b3ec19d16f2f272a7d9a9203034d52f51f7026
tmlx1990 <tmlx1990@163.com>
2024-03-11 16:32:50 +0800

    hive数据库增加查看表的功能

--- chat2db-server/chat2db-plugins/chat2db-hive/src/main/java/ai/chat2db/plugin/hive/HiveMetaData.java
+++ chat2db-server/chat2db-plugins/chat2db-hive/src/main/java/ai/chat2db/plugin/hive/HiveMetaData.java
@@ -2,6 +2,39 @@ package ai.chat2db.plugin.hive;
 
 import ai.chat2db.spi.MetaData;
 import ai.chat2db.spi.jdbc.DefaultMetaService;
+import ai.chat2db.spi.model.Database;
+import ai.chat2db.spi.model.Schema;
+import ai.chat2db.spi.sql.SQLExecutor;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HiveMetaData extends DefaultMetaService implements MetaData {
+
+    @Override
+    public List<Database> databases(Connection connection) {
+        List<Database> databases = new ArrayList<>();
+        return SQLExecutor.getInstance().execute(connection,"show databases", resultSet -> {
+            try {
+                if (resultSet.next()) {
+                    String databaseName = resultSet.getString("database_name");
+                    Database database = new Database();
+                    database.setName(databaseName);
+                    databases.add(database);
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            return databases;
+        });
+    }
+
+    @Override
+    public List<Schema> schemas(Connection connection, String databaseName) {
+        List<Schema> schemas = new ArrayList<>();
+        return schemas;
+    }
 }
+
