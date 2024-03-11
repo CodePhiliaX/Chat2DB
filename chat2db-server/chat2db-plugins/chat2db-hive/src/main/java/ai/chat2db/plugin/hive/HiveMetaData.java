332453f5149417fc5c8253f9274c9bf3edf281b8
tmlx1990 <tmlx1990@163.com>
2024-03-11 18:18:47 +0800

    实现hive查看建表DDL

--- chat2db-server/chat2db-plugins/chat2db-hive/src/main/java/ai/chat2db/plugin/hive/HiveMetaData.java
+++ chat2db-server/chat2db-plugins/chat2db-hive/src/main/java/ai/chat2db/plugin/hive/HiveMetaData.java
@@ -5,6 +5,7 @@ import ai.chat2db.spi.jdbc.DefaultMetaService;
 import ai.chat2db.spi.model.Database;
 import ai.chat2db.spi.model.Schema;
 import ai.chat2db.spi.sql.SQLExecutor;
+import jakarta.validation.constraints.NotEmpty;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -35,6 +36,31 @@ public class HiveMetaData extends DefaultMetaService implements MetaData {
     public List<Schema> schemas(Connection connection, String databaseName) {
         List<Schema> schemas = new ArrayList<>();
         return schemas;
+    }
+
+    @Override
+    public String tableDDL(Connection connection, @NotEmpty String databaseName, String schemaName,
+                           @NotEmpty String tableName) {
+        String sql = "SHOW CREATE TABLE " + format(databaseName) + "."
+                + format(tableName);
+        return SQLExecutor.getInstance().execute(connection, sql, resultSet -> {
+            StringBuilder sb = new StringBuilder();
+            while (resultSet.next()) {
+                // 拼接建表语句
+                sb.append(resultSet.getString("createtab_stmt"));
+                sb.append("\r\n");
+            }
+            if (sb.length() > 0) {
+                sb = sb.delete(sb.length() - 2, sb.length());
+                sb.append(";");
+                return sb.toString();
+            }
+            return null;
+        });
+    }
+
+    public static String format(String name) {
+        return "`" + name + "`";
     }
 }
 
