c2c4f68666022d521388ddd7bcb09102d73153b1
zgq <203083679@qq.com>
2024-03-17 14:04:42 +0800

    fix

--- chat2db-server/chat2db-plugins/chat2db-mongodb/src/main/java/ai/chat2db/plugin/mongodb/MongodbManage.java
+++ chat2db-server/chat2db-plugins/chat2db-mongodb/src/main/java/ai/chat2db/plugin/mongodb/MongodbManage.java
@@ -1,21 +1,21 @@
 package ai.chat2db.plugin.mongodb;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-
 import ai.chat2db.spi.DBManage;
 import ai.chat2db.spi.jdbc.DefaultDBManage;
 import ai.chat2db.spi.sql.Chat2DBContext;
 import ai.chat2db.spi.sql.ConnectInfo;
 import ai.chat2db.spi.sql.SQLExecutor;
-import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.util.StringUtils;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Objects;
 
 public class MongodbManage extends DefaultDBManage implements DBManage {
     @Override
     public void connectDatabase(Connection connection, String database) {
         ConnectInfo connectInfo = Chat2DBContext.getConnectInfo();
-        if (ObjectUtils.anyNull(connectInfo) || StringUtils.isEmpty(connectInfo.getSchemaName())) {
+        if (Objects.isNull(connectInfo) || StringUtils.hasText(connectInfo.getSchemaName())) {
             return;
         }
         String schemaName = connectInfo.getSchemaName();
