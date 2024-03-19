81ae143ba76da5915232913d7ee83d83262242c2
zgq <203083679@qq.com>
2024-03-19 19:38:01 +0800

    h2-database-export

--- chat2db-server/chat2db-plugins/chat2db-h2/src/main/java/ai/chat2db/plugin/h2/H2DBManage.java
+++ chat2db-server/chat2db-plugins/chat2db-h2/src/main/java/ai/chat2db/plugin/h2/H2DBManage.java
@@ -16,33 +16,11 @@ public class H2DBManage extends DefaultDBManage implements DBManage {
     @Override
     public String exportDatabase(Connection connection, String databaseName, String schemaName, boolean containData) throws SQLException {
         StringBuilder sqlBuilder = new StringBuilder();
-        exportTablesAndViews(connection, schemaName, sqlBuilder, containData);
-//        exportProcedures(connection, sqlBuilder);
-//        exportTriggers(connection, sqlBuilder);
-//        exportFunctions(connection, databaseName, sqlBuilder);
+        exportSchema(connection, schemaName, sqlBuilder, containData);
         return sqlBuilder.toString();
     }
 
-    private void exportFunctions(Connection connection, String databaseName, StringBuilder sqlBuilder) throws SQLException {
-        try (ResultSet resultSet = connection.getMetaData().getFunctions(databaseName, null, null)) {
-            while (resultSet.next()) {
-                exportFunction(connection, resultSet.getString("FUNCTION_NAME"), sqlBuilder);
-            }
-
-        }
-    }
-
-    private void exportFunction(Connection connection, String functionName, StringBuilder sqlBuilder) throws SQLException {
-        String sql = String.format("SHOW CREATE FUNCTION %s;", functionName);
-        try (ResultSet resultSet = connection.createStatement().executeQuery(sql)) {
-            if (resultSet.next()) {
-                sqlBuilder.append("DROP FUNCTION IF EXISTS ").append(functionName).append(";").append("\n")
-                        .append(resultSet.getString("Create Function")).append(";").append("\n");
-            }
-        }
-    }
-
-    private void exportTablesAndViews(Connection connection, String schemaName, StringBuilder sqlBuilder, boolean containData) throws SQLException {
+    private void exportSchema(Connection connection, String schemaName, StringBuilder sqlBuilder, boolean containData) throws SQLException {
         String sql = String.format("SCRIPT NODATA NOPASSWORDS NOSETTINGS DROP SCHEMA %s;", schemaName);
         if (containData) {
             sql = sql.replace("NODATA", "");
@@ -57,48 +35,6 @@ public class H2DBManage extends DefaultDBManage implements DBManage {
             }
         }
 
-    }
-
-
-    private void exportProcedures(Connection connection, StringBuilder sqlBuilder) throws SQLException {
-        String sql = "SHOW PROCEDURE STATUS WHERE Db = DATABASE()";
-        try (ResultSet resultSet = connection.createStatement().executeQuery(sql)) {
-            while (resultSet.next()) {
-                exportProcedure(connection, resultSet.getString("Name"), sqlBuilder);
-            }
-        }
-    }
-
-    private void exportProcedure(Connection connection, String procedureName, StringBuilder sqlBuilder) throws SQLException {
-        String sql = String.format("show create procedure %s ", procedureName);
-        try (ResultSet resultSet = connection.createStatement().executeQuery(sql)) {
-            if (resultSet.next()) {
-                sqlBuilder.append("DROP PROCEDURE IF EXISTS ").append(procedureName).append(";").append("\n")
-                        .append("delimiter ;;").append("\n").append(resultSet.getString("Create Procedure")).append(";;")
-                        .append("\n").append("delimiter ;").append("\n");
-            }
-        }
-    }
-
-    private void exportTriggers(Connection connection, StringBuilder sqlBuilder) throws SQLException {
-        String sql = "SHOW TRIGGERS";
-        try (ResultSet resultSet = connection.createStatement().executeQuery(sql)) {
-            while (resultSet.next()) {
-                String triggerName = resultSet.getString("Trigger");
-                exportTrigger(connection, triggerName, sqlBuilder);
-            }
-        }
-    }
-
-    private void exportTrigger(Connection connection, String triggerName, StringBuilder sqlBuilder) throws SQLException {
-        String sql = String.format("show create trigger %s ", triggerName);
-        try (ResultSet resultSet = connection.createStatement().executeQuery(sql)) {
-            if (resultSet.next()) {
-                sqlBuilder.append("DROP TRIGGER IF EXISTS ").append(triggerName).append(";").append("\n")
-                        .append("delimiter ;;").append("\n").append(resultSet.getString("SQL Original Statement")).append(";;")
-                        .append("\n").append("delimiter ;").append("\n");
-            }
-        }
     }
 
     @Override
