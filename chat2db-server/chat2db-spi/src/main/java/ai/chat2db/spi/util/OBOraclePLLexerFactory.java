26180fccdeed875ed0fce641779c77c38069233b
SwallowGG <1558143046@qq.com>
2024-05-31 14:44:38 +0800

    sql parse

---
+++ chat2db-server/chat2db-spi/src/main/java/ai/chat2db/spi/util/OBOraclePLLexerFactory.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2023 OceanBase.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ai.chat2db.spi.util;
+
+import com.oceanbase.tools.sqlparser.oboracle.PLLexer;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.Lexer;
+
+class OBOraclePLLexerFactory implements LexerFactory {
+    @Override
+    public Lexer create(CharStream input) {
+        return new PLLexer(input);
+    }
+}
