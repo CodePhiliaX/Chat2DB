5e1d251a2dbd6f7b6b34737ae86e55e9e61eefe9
psxjoy <psxjoy@outlook.com>
2024-11-19 14:21:29 +0800

    fix:fix some typos in project

--- chat2db-server/chat2db-server-test/src/test/java/ai/chat2db/server/test/temp/SqlTest.java
+++ chat2db-server/chat2db-server-test/src/test/java/ai/chat2db/server/test/temp/SqlTest.java
@@ -26,7 +26,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
-public class SqlTset extends BaseTest {
+public class SqlTest extends BaseTest {
 
     @Resource
     private TableService tableService;
