e10aafef7fba2c9cbfed1d0765571a148a6fae49
hkjang <gagagiga@naver.com>
2024-12-05 14:04:03 +0900

    add ko-kr Korean language

---
+++ chat2db-client/src/i18n/ko-kr/dashboard.ts
@@ -0,0 +1,12 @@
+export default {
+  "dashboard.title": "대시보드",
+  "dashboard.edit": "편집",
+  "dashboard.modal.editTitle": "대시보드 편집",
+  "dashboard.modal.addTitle": "대시보드 추가",
+  "dashboard.modal.name.placeholder": "대시보드 이름을 입력하세요",
+  "dashboard.delete": "삭제",
+  "dashboard.export2image": "이미지로 내보내기",
+  "dashboard.editor.cascader.placeholder": "연결을 선택하세요",
+  "dashboard.editor.execute.noDataSource": "먼저 데이터 소스를 선택하세요",
+  "dashboard.editor.execute.success": "실행 성공. 차트 설정을 선택하세요"
+}
