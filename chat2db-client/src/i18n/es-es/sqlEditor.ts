8e6d6c02890915a529403c29bc30bb79a89e4053
Adrc95 <awinf204@gmail.com>
2025-08-22 11:14:48 +0200

    added spanish language

---
+++ chat2db-client/src/i18n/es-es/sqlEditor.ts
@@ -0,0 +1,9 @@
+export default {
+  'sqlEditor.text.keyword': 'Palabra clave',
+  'sqlEditor.text.function': 'Función',
+  'sqlEditor.text.tableName': 'Nombre de la tabla',
+  'sqlEditor.text.databaseName': 'Nombre de la base de datos',
+  'sqlEditor.text.schemaName': 'Esquema',
+  'sqlEditor.text.viewName': 'Vista',
+  'sqlEditor.text.fieldName': 'Nombre del campo',
+};
