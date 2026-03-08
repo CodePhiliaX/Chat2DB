15ec66005e92690dbbddc0018ea14248c35bc943
robinji0 <137188352+robinji0@users.noreply.github.com>
2026-03-08 11:13:07 +0800

    Add git filter-branch command to remove directory
    Add a command to remove a directory from git history.
    
    Signed-off-by: robinji0 <137188352+robinji0@users.noreply.github.com>

--- document/git/git.sh
+++ document/git/git.sh
@@ -1,6 +1,7 @@
 # https://www.zhangbj.com/p/1437.html
 # -r represents recursion
 #
+#
 git filter-branch --force --index-filter 'git rm -r --cached --ignore-unmatch chat2db-client/static' --prune-empty --tag-name-filter cat -- --all
 git push origin developing  --force
 git push origin delete_git  --force
